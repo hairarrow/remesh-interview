d0edc667ac710992e5d9905841a9d5099f28513c
Emmanuel Herrero <emmanuelherrero@gmail.com>
2018-09-06 08:37:23 -0500

    100 on server

--- __tests__/backend/app.test.js
+++ __tests__/backend/app.test.js
@@ -1,6 +1,29 @@
 import request from "supertest";
 import app from "../../backend/app";
 
+describe("Test static server", () => {
+  const OLD_ENV = process.env;
+
+  beforeEach(() => {
+    jest.resetModules();
+    process.env = { ...OLD_ENV };
+    delete process.env.NODE_ENV;
+  });
+
+  afterEach(() => {
+    process.env = OLD_ENV;
+  });
+
+  test("will receive process.env", () => {
+    process.env.NODE_ENV = "production";
+    process.env.PORT = 5000;
+
+    const prodApp = require("../../backend/app").default;
+
+    console.log(prodApp);
+  });
+});
+
 describe("Test api paths", () => {
   describe("Test /api/users", () => {
     test("It should response the GET method", done => {
